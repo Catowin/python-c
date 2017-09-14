7737ab626b32885c1e57e8d53ad781073610d67a
Catowin <catowin.alia@gmail.com>
2017-09-14 11:31:12 +0300

    init commit

--- blog/urls.py
+++ blog/urls.py
@@ -18,5 +18,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'), # Что указать (рег) и связать с вьюз постлист  + свяжем с ф-ц постлист
 ]
 # Все что записали после /блог перейдет в админку
